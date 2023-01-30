18b4a0bfc275bdb645f8cf0403023a566607e4a9
Q1.11Wiesenthal <Q1.11Wiesenthal@R200C09.SGNW.local>
2023-01-30 11:23:46 +0100

    Added Interface ComperableContent and multiple methods in Binary search Tree

--- BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
+++ BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -8,16 +8,90 @@ package binarysearchtree;
  *
  * @author Q1.11Wiesenthal
  */
-public class BinarySearchTree {
+public class BinarySearchTree<T extends ComperableContent<T>> {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private BinarySearchTree<T> leftTree;
+    private BinarySearchTree<T> rightTree;
+    private T content;
+
+    public BinarySearchTree() {
     }
-public boolean isEmpty(){
-return false;
-}
-    
+
+    public boolean isEmpty() {
+        return content != null;
+    }
+
+    public void insert(T pContent) {
+        if (pContent != null) {
+            if (!isEmpty()) {
+                if (pContent.isEqual(content)) {
+                    return;
+                } else if (pContent.isLess(content)) {
+                    leftTree.insert(pContent);
+                } else if (pContent.isGreater(pContent)) {
+                    rightTree.insert(pContent);
+                }
+            }
+            content = pContent;
+        }
+    }
+
+    public T search(T pContent) {
+        if (!isEmpty() && pContent != null) {
+            if (pContent.isEqual(content)) {
+                return content;
+            } else if (pContent.isLess(content)) {
+                leftTree.search(pContent);
+            } else if (pContent.isGreater(pContent)) {
+                rightTree.search(pContent);
+            }
+        }
+        return null;
+    }
+
+    public void remove(T pContent) {
+        int c;
+        char direction;
+        BinarySearchTree<T> parent = null;
+        if (!isEmpty() && pContent != null) {
+            if (pContent.isEqual(content)) {
+                if (getLeftTree().isEmpty() && getRightTree().isEmpty()) {
+                    c = 1;
+                } else if (getLeftTree().isEmpty()) {
+                    c = 2;
+                    direction = 'l';
+                } else if (getRightTree().isEmpty()) {
+                    c = 2;
+                    direction = 'r';
+                } else {
+                    c = 3;
+                }
+                switch (c) {
+                    case 1:
+                    case 2:
+                    case 3:
+                }
+            }
+        } else if (pContent.isLess(content)) {
+            parent = this;
+            leftTree.remove(pContent);
+        } else if (pContent.isGreater(pContent)) {
+            parent = this;
+            rightTree.remove(pContent);
+        }
+    }
+
+
+    public T getContent() {
+        return content;
+    }
+
+    public BinarySearchTree<T> getLeftTree() {
+        return leftTree;
+    }
+
+    public BinarySearchTree<T> getRightTree() {
+        return rightTree;
+    }
+
 }
---
+++ BinarySearchTree/src/binarysearchtree/ComperableContent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package binarysearchtree;
+
+/**
+ *
+ * @author Q1.11Wiesenthal
+ */
+public interface ComperableContent<T> {
+
+    public boolean isGreater(T pContent);
+    public boolean isLess(T pContent);
+    public boolean isEqual(T pContent);
+
+}
